61448cc68efb230fbd464b3468b6b6cf517228d3
AngelACrawford <aac72646@uga.edu>
2023-03-30 09:07:36 -0400

    Just they layout

---
+++ app/src/main/java/edu/uga/cs/countryquiz/HomePageActivity.java
@@ -0,0 +1,6 @@
+package edu.uga.cs.countryquiz;
+
+import android.app.Activity;
+
+public class HomePageActivity extends Activity {
+}
---
+++ app/src/main/java/edu/uga/cs/countryquiz/ViewPastQuizzes.java
@@ -0,0 +1,6 @@
+package edu.uga.cs.countryquiz;
+
+import android.app.Activity;
+
+public class ViewPastQuizzes extends Activity {
+}
